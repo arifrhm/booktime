3bcacfab0e98056d16c426f3453bb8fadaf5e5b3
Arif Rahman <arif.rahman.rhm@gmail.com>
2024-02-27 23:49:13 +0700

    Feat : Add models, signals, and tests

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -4,6 +4,8 @@ from django.test import TestCase
 # We need to remove the hard-coded URLs inside the tests. This would
 # be the final version of main/tests.py:
 from django.urls import reverse
+from main import forms
+
 
 class TestPage(TestCase):
     def test_home_page_works(self):
@@ -17,3 +19,12 @@ class TestPage(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'about_us.html')
         self.assertContains(response, 'BookTime')
+
+    def test_contact_us_page_works(self):
+        response = self.client.get(reverse("contact_us"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/contact_form.html')
+        self.assertContains(response, 'BookTime')
+        self.assertIsInstance(
+            response.context["form"], forms.ContactForm
+        )
--- main/views.py
+++ main/views.py
@@ -1,5 +1,53 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from main import forms
+from django.http import HttpResponseRedirect
+from django.views.generic.list import ListView
+from django.shortcuts import get_object_or_404
+from main import models
+
 
 # Create your views here.
 def home(request):
     return render(request, "home.html", {})
+
+
+class ContactUsView(FormView):
+    template_name = "contact_form.html"
+    form_class = forms.ContactForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        form.send_mail()
+        return super().form_valid(form)
+
+
+def contact_us(request):
+    if request.method == 'POST':
+        form = forms.ContactForm(request.POST)
+        if form.is_valid():
+            form.send_mail()
+            return HttpResponseRedirect('/')
+        else:
+            form = forms.ContactForm()
+    return render(request, 'contact_form.html', {'form': form})
+
+
+class ProductListView(ListView):
+    template_name = "main/product_list.html"
+    paginate_by = 4
+
+    def get_queryset(self):
+        tag = self.kwargs['tag']
+        self.tag = None
+        if tag != "all":
+            self.tag = get_object_or_404(
+                models.ProductTag, slug=tag
+            )
+        if self.tag:
+            products = models.Product.objects.active().filter(
+                tags=self.tag
+            )
+        else:
+            products = models.Product.objects.active()
+            return products.order_by("name")
