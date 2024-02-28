8595a12a718ae40531c7a67200cf0c5957446246
Arif Rahman <arif.rahman@delameta.com>
2024-02-28 11:38:10 +0700

    Page 146

--- main/tests/test_forms.py
+++ main/tests/test_forms.py
@@ -8,7 +8,7 @@ class TestForm(TestCase):
         form = forms.ContactForm({
             "name": "Luke Skywalker",
             "message": "Hi there"
-            })
+        })
 
         self.assertTrue(form.is_valid())
 
@@ -21,3 +21,22 @@ class TestForm(TestCase):
     def test_invalid_contact_us_form(self):
         form = forms.ContactForm({"message": "Hi there"})
         self.assertFalse(form.is_valid())
+
+    def test_valid_signup_form_sends_email(self):
+        form = forms.UserCreationForm(
+            {
+                "email": "user@domain.com",
+                "password1": "abcabcabc",
+                "password2": "abcabcabc",
+            }
+        )
+        self.assertTrue(form.is_valid())
+
+        with self.assertLogs("main.forms", level="INFO") as cm:
+            form.send_mail()
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(
+            mail.outbox[0].subject, "Welcome to BookTime"
+        )
+        self.assertGreaterEqual(len(cm.output), 1)
--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,8 @@ from django.views.generic import TemplateView
 from main import views
 from django.views.generic.detail import DetailView
 from main import models
+from django.contrib.auth import views as auth_views
+from main import forms
 
 urlpatterns = [
     path(
@@ -13,7 +15,7 @@ urlpatterns = [
         "",
         TemplateView.as_view(template_name="home.html"),
         name="home",
-        ),
+    ),
     path(
         "contact-us/",
         views.ContactUsView.as_view(),
@@ -28,5 +30,38 @@ urlpatterns = [
         "product/<slug:slug>/",
         DetailView.as_view(model=models.Product),
         name="product",
+    ),
+    path(
+        'signup/',
+        views.SignupView.as_view(),
+        name="signup"
+    ),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="login.html",
+            form_class=forms.AuthenticationForm,
+        ),
+        name="login",
+    ),
+    path(
+        "address/",
+        views.AddressListView.as_view(),
+        name="address_list",
+    ),
+    path(
+        "address/create/",
+        views.AddressCreateView.as_view(),
+        name="address_create",
+    ),
+    path(
+        "address/<int:pk>/",
+        views.AddressUpdateView.as_view(),
+        name="address_update",
+    ),
+    path(
+        "address/<int:pk>/delete/",
+        views.AddressDeleteView.as_view(),
+        name="address_delete",
     ),
 ]
